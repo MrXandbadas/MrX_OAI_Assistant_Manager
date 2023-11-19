b53dcc9f2d24026ca48a29d7114b174e0c50c5f2
Kodie Amos <kodiea@gmail.com>
2023-11-19 15:00:49 +1100

    Update assistant_manager.py
    Make it a bit more self contained

--- assistant_manager.py
+++ assistant_manager.py
@@ -9,7 +9,6 @@ import json
 import dynamic_functions
 from utils.file_operations import read_file, write_file, exec_python, exec_sh
 from utils.special_functions import get_stock_price, generate_image, create_image_variation, edit_image
-
 class OAI_Assistant():
     def __init__(self, api_key, organization, timeout=None, log_level=logging.INFO):
         """
@@ -965,6 +964,27 @@ class OAI_Assistant():
                     extra_body=extra_body,
                     timeout=timeout
             )
+    
+    def cancel_run(self, thread_id, run_id, extra_headers=None, extra_query=None, extra_body=None, timeout=None):
+            """
+            Cancels a run.
+
+            Args:
+                thread_id: The ID of the thread the run belongs to.
+                run_id: The ID of the run to cancel.
+                extra_headers: Send extra headers
+                extra_query: Add additional query parameters to the request
+                extra_body: Add additional JSON properties to the request
+                timeout: Override the client-level default timeout for this request, in seconds
+            """
+            return self.client.threads.runs.cancel(
+                    thread_id=thread_id,
+                    run_id=run_id,
+                    extra_headers=extra_headers,
+                    extra_query=extra_query,
+                    extra_body=extra_body,
+                    timeout=timeout
+            )
 
 
     def submit_tool_outputs(self, thread_id, run_id, tool_outputs):
@@ -996,7 +1016,7 @@ class OAI_Assistant():
         self.logger.debug(f"Submitted tool outputs for run {run_id}")
 
         return run
-
+    
     def process_run(self,thread_id, run_id):
         while True:
             run = self.retrieve_run(thread_id, run_id)
@@ -1048,12 +1068,39 @@ class OAI_Assistant():
                         arguments = json.loads(action["function"]["arguments"])
                         function_output = edit_image(self, **arguments)
                         tools_output.append({"tool_call_id": action["id"], "output": str(function_output)})
+                    elif action["function"]["name"] == "append_new_tool_function_and_metadata":
+                        arguments = json.loads(action["function"]["arguments"])
+                        # get the function name
+                        function_name = arguments["function_name"]
+                        # get the function code
+                        function_code = arguments["function_code"]
+                        # get the metadata dict
+                        function_metadata = arguments["metadata_dict"]
+                        #Check if we need to json.loads the metadata
+                        if isinstance(function_metadata, str):
+                            function_metadata = json.loads(arguments["metadata_dict"])
+                        #print(f"Function name: {function_name}")
+
+                        print(f"Function metadata: {function_metadata}")
+                        # append the function and metadata to the current assistant
+
+                    
+                        function_output = append_new_tool_function_and_metadata(function_name, function_code, function_metadata)
+
+                        #function_output = append_new_tool_function_and_metadata(self, **(arguments))
+                        tools_output.append({"tool_call_id": action["id"], "output": str(function_output)})
                     elif action["function"]["name"] in dir(dynamic_functions):
                         
                         arguments = json.loads(action["function"]["arguments"])
                         function_name = action["function"]["name"]
                         function = getattr(dynamic_functions, function_name)
-                        function_output = function(**arguments)
+                        # Wrap the function call in a try except block so we can hand the error back to as an output
+                        
+                        #check if the arguments are a string and if so convert to dict
+                        
+                        function_output = function(**(arguments))
+                    
+                        
                         tools_output.append({"tool_call_id": action["id"], "output": str(function_output)})
                     else:
                         print(f"Function {action['function']['name']} not found")
@@ -1062,8 +1109,220 @@ class OAI_Assistant():
 
             elif run.status == "failed":
                 print("The run failed.")
-                print(f"Error: {json.dumps(run, indent=4)}")
+                print(f"Error: {json.dumps(str(run), indent=4)}")
                 return None
             else:
                 time.sleep(2)
                 continue
+    def message_user(self, message):
+        """Overwrite this function to change how the user is messaged"""
+        print(message)
+
+    def get_user_input(self):
+        """Overwrite this function to change how the user is messaged"""
+        # Get the input from the user
+        return input("User: ")
+    
+    def get_multiple_choice_input(self, choices):
+        """Overwrite this function to change how the user is messaged"""
+        # Display the options with corresponding numbers
+        options = [f"{i+1}. {choice}" for i, choice in enumerate(choices)]
+        self.message_user(f"Please select one of the following options:\n{', '.join(options)}")
+        
+        # Validate the user's input
+        while True:
+            choice = input("User: ")
+            if choice.isdigit() and 1 <= int(choice) <= len(choices):
+                return choices[int(choice) - 1]
+            else:
+                self.message_user("Invalid choice. Please enter a valid number.")
+    
+    def setup_thread(self, input_thread_name=None, input_thread_id=None) -> int:
+        # Create a new thread if thread_id is None
+        
+        if input_thread_name is not None:
+            thread_id = self.change_thread(input_thread_name)
+        elif input_thread_id is not None:
+            #change the thread to the thread with the given ID
+            thread_id = self.change_thread(thread_id=input_thread_id)
+        else:
+            #create a thread with the deafult name
+            thread_id = self.change_thread(thread_name="Default_Thread")
+
+
+        self.current_thread = thread_id
+        self.prepare_thread_history(thread_id=thread_id)
+        return thread_id
+    
+    def swap_Thread(self):
+        #Ask the user for the thread name or ID
+        options = ["Name", "ID", "Multiple Choice (Save Locally)"]
+        selected = self.get_multiple_choice_input(options)
+        # If the user selected name, ask for the name
+        if selected == "Name":
+            self.message_user("Please enter the name of the thread")
+            thread_name = self.get_user_input
+            thread_id = self.setup_thread(self, input_thread_name=thread_name)
+            return thread_id
+        # If the user selected ID, ask for the ID
+        elif selected == "ID":
+            self.message_user("Please enter the ID of the thread")
+            thread_id = self.get_user_input()
+            thread_id = self.setup_thread(self, input_thread_id=thread_id)
+            return thread_id
+        # If the user selected Multiple Choice, Provide a list of threads and ask the user to select one
+        elif selected == "Multiple Choice (Save Locally)":
+            #load the threads in teh json file
+            with open('thread_ids.json') as json_file:
+                data = json.load(json_file)
+                local_threads = []
+                for thread_name, thread_id in data.items():
+                    local_threads.append(thread_name)
+
+                selected = self.get_multiple_choice_input(local_threads)
+                thread_id = data.get(selected)
+                if thread_id is None:
+                    self.message_user(f"No thread found with name {selected}")
+                    return
+
+                thread_id = self.setup_thread(self, input_thread_id=thread_id)
+
+                #check for messages in the thread
+                history = self.list_thread_history()
+                #message_user(f"History: {history}")
+                if history is not None:
+                    for message in reversed(history):
+                        self.message_user("------------")
+                        data = self.retrieve_message(thread_id=thread_id, message_id=message)
+                        #data.content[0].text.value
+                        self.message_user(f"{data.role}: {data.content[0].text.value}")
+                return thread_id
+            
+    def setup_assistant_chat(self):
+        """
+        Takes assistant API object and returns the ID of the selected assistant Uses chat to facillitate selection process.
+
+        Args:
+            the_assistant (OAI_Assistant): An instance of the OAI_Assistant class.
+
+        Returns:
+            str: The ID of the selected assistant.
+        """
+        # Lets start by getting the list of assistants
+        assistants = self.assistants
+        # Print list in a readable format it is a SyncCursorPage
+        #message_user('List of assistants:')
+        #message_user(f"{assistants}")
+        local_assistants = []
+        for i, assistant in enumerate(assistants.data):
+            local_assistants.append(assistant.name)
+
+        # Lets ask the user to select an assistant
+        selected = self.get_multiple_choice_input(local_assistants)
+
+        self.message_user(f"You selected {selected}")
+        self.message_user("Lets begin the chat")
+
+        # Lets grab the assistant ID
+        assistant_id = assistants.data[local_assistants.index(selected)].id
+        self.assistant_id = assistant_id
+        self.message_user(f"Assistant ID: {assistant_id}")
+
+        return assistant_id
+
+    def main_run(self, assistant_id,thread_id):
+        while True:
+            # Get the input from the user
+            # Remind the users of their chat controls
+
+            self.message_user("""------------
+                              Your chat controls are as follows:
+                              To quit the chat enter 'Q'/'q' | To start a new thread enter 'swapT' | To swap assistants enter 'swapA'
+                              Please enter your message or a chat control.
+                              ------------
+                              """)
+            message = self.get_user_input()
+            #Check the users response for logic commands
+            # Q for quit, new for new thread. takes one arg "thread Name" and saved the ID to the assistant object appropriately
+            # swapA is for swapping the assistant, it initiates the agent selection process again
+
+            if message == "Q" or message == "q":
+                break
+            elif message == "tool":
+                self.message_user("Enabling tools")
+                #Grab the tools from the assistant function metadata
+                tools = self.load_tool_metadata()
+                choices = self.get_multiple_choice_input(tools)
+                print(choices)
+                #Collect the information about the selection. int has been returned which we need to use to grab the correct dict item
+                tools_list = []
+                correct_info = [{
+                    "type": "function",
+                    "function": {}
+                }]
+                
+                #metadata = {}
+                #tools is a dict and choices is a int, we need to grab the correct item via the int
+                #grab the item from the dict in a way that uses the int. We need to convert the dict to a list first
+                for choice in choices:
+                    #grab the correct info from the list
+                    tool_name = choice["tool_name"]
+                    tool_required = choice["tool_required"]
+                    tool_description = choice["tool_description"]
+                    tool_properties = choice["tool_properties"]
+                    tool_metadata = self.make_tool_metadata(tool_name=tool_name, tool_required=tool_required, tool_description=tool_description, tool_properties=tool_properties)
+                    # add the tool to the metadata dict
+                    correct_info = {
+                    "type": "function",
+                    "function": tool_metadata
+                    }
+                    #add the tool to the tools list
+                    tools_list.append(correct_info)
+                    
+                #Check if the user wants to enable the tools
+                self.message_user("Are you sure you want to enable these tools? (Y/N)")
+                choice = self.get_multiple_choice_input(["Y", "N"])
+
+                if choice == "Y":
+                    #enable the tools
+                    
+                    assistant_new = self.enable_tools(assistant_id, tools_list)
+                    self.assistant_id = assistant_new.id
+
+                else:
+                    self.message_user("Tools not enabled")
+                
+                continue
+            elif message == "swapT":
+                thread_swapped = self.swap_Thread()
+
+                if thread_swapped is not None:
+                    thread_id = thread_swapped
+                    self.current_thread = thread_id
+                continue
+
+            elif message == "swapA":
+                assistant_id = self.setup_assistant_chat()
+                continue
+
+            ThreadMessage = self.create_message(thread_id=thread_id, role="user", content=message)
+            user_message_id = ThreadMessage.id
+            self.chat_ids.append(user_message_id)
+            run = self.create_run(thread_id=thread_id, assistant_id=assistant_id)
+            run_done = self.process_run(thread_id=thread_id, run_id=run.id)
+            #Wait for the run to complete
+            if run_done is not None:
+                print("Run Completed")
+                message_list = self.list_thread_history()
+                #check for a new message in the thread
+                for message in message_list:
+                        message_obj = self.retrieve_message(thread_id=thread_id, message_id=message)
+                        if message_obj.id in self.chat_ids:
+                            continue
+                        else:
+                            self.message_user(f'assistant: {message_obj.content[0].text.value}')
+                            self.chat_ids.append(message_obj.id)
+
+            else:
+                print(f"Else out? {run.status}")
+    

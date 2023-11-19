bcd293379a2a2648f23cb83a325d3bb474654b9a
Kodie Amos <kodiea@gmail.com>
2023-11-19 15:01:58 +1100

    Clean up time
    well it look much neater now everything is contained within the class structure

--- CMD_Agent_Chat.py
+++ CMD_Agent_Chat.py
@@ -8,264 +8,15 @@ org_id = os.environ.get('ORG_ID')
 api_key = os.environ.get('API_KEY')
 
 
-#### General Helper Functions ####
-
-def message_user(message):
-    print(message)
-
-def get_input():
-    message_user("------------")
-    return input('>>> ')
-
-def get_multiple_choice_input(options):
-    print('Please input Numbers only to select one of the following options or enter Q to leave:')
-    for i, option in enumerate(options):
-        print(f'{i+1}. {option}')
-    while True:
-        try:
-            choice = input('>>> ')
-            if choice == "Q" or choice == "q" or choice == "Quit" or choice == "quit" or choice == "exit":
-                return None
-            choice = int(choice)
-            if choice > len(options) or choice < 1:
-                raise ValueError
-            return options[choice-1]
-        except ValueError:
-            print('Please enter a valid option')
-
-def get_multiple_choice_multiple_input(options:dict):
-    print('Please input Numbers only to select any of the following options or enter Q to leave:')
-    for i, option in enumerate(options):
-        print(f'{i+1}. {option}')
-    while True:
-        try:
-            choice = input('>>> ')
-            if choice.lower() in ["q", "quit", "exit"]:
-                return None
-            if "," in choice:
-                choices = [int(i) - 1 for i in choice.split(",")]
-                if any(choice >= len(options) or choice < 0 for choice in choices):
-                    raise ValueError
-                return [options[list(options.keys())[choice]] for choice in choices]
-            else:
-                choice = int(choice) - 1
-                if choice >= len(options) or choice < 0:
-                    raise ValueError
-                return [options[list(options.keys())[choice]]] # Modified line
-        except ValueError:
-            print('Please enter a valid option')
-
-
-
-#### End of General Helper Functions ####
-
-
-#### chat working functions ####
-
-def setup_assistant_chat(the_assistant: OAI_Assistant):
-    """
-    Takes assistant API object and returns the ID of the selected assistant Uses chat to facillitate selection process.
-
-    Args:
-        the_assistant (OAI_Assistant): An instance of the OAI_Assistant class.
-
-    Returns:
-        str: The ID of the selected assistant.
-    """
-    # Lets start by getting the list of assistants
-    assistants = the_assistant.assistants
-    # Print list in a readable format it is a SyncCursorPage
-    #message_user('List of assistants:')
-    #message_user(f"{assistants}")
-    local_assistants = []
-    for i, assistant in enumerate(assistants.data):
-        local_assistants.append(assistant.name)
-
-    # Lets ask the user to select an assistant
-    selected = get_multiple_choice_input(local_assistants)
-
-    message_user(f"You selected {selected}")
-    message_user("Lets begin the chat")
-
-    # Lets grab the assistant ID
-    assistant_id = assistants.data[local_assistants.index(selected)].id
-    the_assistant.assistant_id = assistant_id
-    message_user(f"Assistant ID: {assistant_id}")
-
-    return assistant_id
-
-def setup_thread(the_assistant: OAI_Assistant, input_thread_name=None, input_thread_id=None) -> int:
-    # Create a new thread if thread_id is None
-    
-    if input_thread_name is not None:
-        thread_id = the_assistant.change_thread(input_thread_name)
-    elif input_thread_id is not None:
-        #change the thread to the thread with the given ID
-        thread_id = the_assistant.change_thread(thread_id=input_thread_id)
-    else:
-        #create a thread with the deafult name
-        thread_id = the_assistant.change_thread(thread_name="Default_Thread")
-
-
-    the_assistant.current_thread = thread_id
-    the_assistant.prepare_thread_history(thread_id=thread_id)
-    return thread_id
-
-
-def swap_Thread(assistant: OAI_Assistant):
-    #Ask the user for the thread name or ID
-    options = ["Name", "ID", "Multiple Choice (Save Locally)"]
-    selected = get_multiple_choice_input(options)
-    # If the user selected name, ask for the name
-    if selected == "Name":
-        message_user("Please enter the name of the thread")
-        thread_name = get_input()
-        thread_id = setup_thread(assistant, input_thread_name=thread_name)
-        return thread_id
-    # If the user selected ID, ask for the ID
-    elif selected == "ID":
-        message_user("Please enter the ID of the thread")
-        thread_id = get_input()
-        thread_id = setup_thread(assistant, input_thread_id=thread_id)
-        return thread_id
-    # If the user selected Multiple Choice, Provide a list of threads and ask the user to select one
-    elif selected == "Multiple Choice (Save Locally)":
-        #load the threads in teh json file
-        with open('thread_ids.json') as json_file:
-            data = json.load(json_file)
-            local_threads = []
-            for thread_name, thread_id in data.items():
-                local_threads.append(thread_name)
-
-            selected = get_multiple_choice_input(local_threads)
-            thread_id = data.get(selected)
-            if thread_id is None:
-                message_user(f"No thread found with name {selected}")
-                return
-
-            thread_id = setup_thread(assistant, input_thread_id=thread_id)
-
-            #check for messages in the thread
-            history = assistant.list_thread_history()
-            #message_user(f"History: {history}")
-            if history is not None:
-                for message in reversed(history):
-                    message_user("------------")
-                    data = assistant.retrieve_message(thread_id=thread_id, message_id=message)
-                    #data.content[0].text.value
-                    message_user(f"{data.role}: {data.content[0].text.value}")
-            return thread_id
-        
-
-#### End of chat working functions ####
-
-def main_run(assistant: OAI_Assistant, assistant_id,thread_id):
-
-    
-
-    while True:
-        # Get the input from the user
-        # Remind the users of their chat controls
-
-        message_user("------------")
-        message_user("Your chat controls are as follows:")
-        message_user("To quit the chat enter 'Q'/'q' | To start a new thread enter 'swapT' | To swap assistants enter 'swapA'")
-        message_user("Please enter your message or a chat control.")
-        message = get_input()
-        message_user("------------")
-        #Check the users response for logic commands
-        # Q for quit, new for new thread. takes one arg "thread Name" and saved the ID to the assistant object appropriately
-        # swapA is for swapping the assistant, it initiates the agent selection process again
-
-        if message == "Q" or message == "q":
-            break
-        elif message == "tool":
-            message_user("Enabling tools")
-            #Grab the tools from the assistant function metadata
-            tools = assistant.load_tool_metadata()
-            choices = get_multiple_choice_multiple_input(tools)
-            print(choices)
-            #Collect the information about the selection. int has been returned which we need to use to grab the correct dict item
-            tools_list = []
-            correct_info = [{
-                "type": "function",
-                "function": {}
-            }]
-            
-            #metadata = {}
-            #tools is a dict and choices is a int, we need to grab the correct item via the int
-            #grab the item from the dict in a way that uses the int. We need to convert the dict to a list first
-            for choice in choices:
-                #grab the correct info from the list
-                tool_name = choice["tool_name"]
-                tool_required = choice["tool_required"]
-                tool_description = choice["tool_description"]
-                tool_properties = choice["tool_properties"]
-                tool_metadata = assistant.make_tool_metadata(tool_name=tool_name, tool_required=tool_required, tool_description=tool_description, tool_properties=tool_properties)
-                # add the tool to the metadata dict
-                correct_info = {
-                "type": "function",
-                "function": tool_metadata
-                }
-                #add the tool to the tools list
-                tools_list.append(correct_info)
-                
-            #Check if the user wants to enable the tools
-            message_user("Are you sure you want to enable these tools? (Y/N)")
-            choice = get_multiple_choice_input(["Y", "N"])
-
-            if choice == "Y":
-                #enable the tools
-                
-                assistant_new = assistant.enable_tools(assistant_id, tools_list)
-                assistant.assistant_id = assistant_new.id
-
-            else:
-                message_user("Tools not enabled")
-            
-            continue
-        elif message == "swapT":
-            thread_swapped = swap_Thread(assistant)
-
-            if thread_swapped is not None:
-                thread_id = thread_swapped
-                assistant.current_thread = thread_id
-            continue
-
-        elif message == "swapA":
-            assistant_id = setup_assistant_chat(assistant)
-            continue
-
-        ThreadMessage = assistant.create_message(thread_id=thread_id, role="user", content=message)
-        user_message_id = ThreadMessage.id
-        assistant.chat_ids.append(user_message_id)
-        run = assistant.create_run(thread_id=thread_id, assistant_id=assistant_id)
-        run_done = assistant.process_run(thread_id=thread_id, run_id=run.id)
-        #Wait for the run to complete
-        if run_done is not None:
-            print("Run Completed")
-            message_list = assistant.list_thread_history()
-            #check for a new message in the thread
-            for message in message_list:
-                    message_obj = assistant.retrieve_message(thread_id=thread_id, message_id=message)
-                    if message_obj.id in assistant.chat_ids:
-                        continue
-                    else:
-                        message_user(f'assistant: {message_obj.content[0].text.value}')
-                        assistant.chat_ids.append(message_obj.id)
-
-        else:
-            print(f"Else out? {run.status}")
-
 #### Main Program ####
 # Lets connect to openAI assistant Endpoints
 assistant = OAI_Assistant(api_key=api_key, organization=org_id)
 
 # Interact with the user to select an assistant
-assistant_id = setup_assistant_chat(assistant)
+assistant_id = assistant.setup_assistant_chat()
 
 # Interact with the user to select a thread
-thread_id = swap_Thread(assistant)
+thread_id = assistant.swap_Thread()
 
 
 
@@ -274,14 +25,14 @@ history = assistant.list_thread_history()
 #message_user(f"History: {history}")
 if history is not None:
     for message in reversed(history):
-        message_user("------------")
+        assistant.message_user("------------")
         data = assistant.retrieve_message(thread_id=thread_id, message_id=message)
         #data.content[0].text.value
-        message_user(f"{data.role}: {data.content[0].text.value}")
+        assistant.message_user(f"{data.role}: {data.content[0].text.value}")
 
 
 #start the chat
-message_user("------------")
-message_user("Your Chat has begun")
+assistant.message_user("------------")
+assistant.message_user("Your Chat has begun")
 
-main_run(assistant, assistant_id,thread_id)
+assistant.main_run(assistant, assistant_id,thread_id)

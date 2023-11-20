b02f30b3fb12694203195d4c10c078a549bf4b9a
Kodie Amos <kodiea@gmail.com>
2023-11-20 19:09:09 +1100

    Update special_functions.py
    re-tooling

--- utils/special_functions.py
+++ utils/special_functions.py
@@ -91,3 +91,23 @@ def create_image_variation(assistant, image_path, n=1, size='1024x1024'):
     except Exception as e:
         print(f"An error occurred: {e}")
         return None
+
+    # Function to append a new tool function and its metadata
+def append_new_tool_function_and_metadata(function_name: str, function_code: str, metadata: dict):
+    try:
+        # Logic to append new function code to dynamic_functions.py
+        with open('dynamic_functions.py', 'a') as file:
+            file.write(f'\n\n{function_code}')
+
+        # Logic to append new metadata to functions_metadata.json
+        with open('functions_metadata.json', 'r+') as file:
+            existing_metadata = json.load(file)
+            # Lets run a check to see if the read metadata is hiding in a dict wrapped around our dict
+            
+            existing_metadata[function_name] = metadata
+            file.seek(0)  # Reset file position to the beginning.
+            json.dump(existing_metadata, file, indent=4)
+    except Exception as e:
+        print(f"An error occurred while appending the new function: {e}")
+        return False
+    return True  # Indication that the function and metadata have been successfully appended
